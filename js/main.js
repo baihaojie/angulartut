5452d801b9221ee9df511c444c9c70d991480fe0
brittanyn <brittanyarielle@gmail.com>
2015-08-09 23:30:28 +0000

    Completed part 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var app = angular.module('myApp', []);
 
+var apiKey = 'MDIwMTMxMjUwMDE0MzkxNjI2MjEzOGVmYQ001',
+    nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
+
 app.controller('PlayerController', ['$scope', function($scope) {
     $scope.playing = false;
     $scope.audio = document.createElement('audio');
@@ -16,6 +19,15 @@ app.controller('PlayerController', ['$scope', function($scope) {
         $scope.$apply(function() {
             $scope.stop()
         });
+    });
+    
+    $http({
+        method: 'JSONP',
+        url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+    }).success(function(data, status) {
+    
+    }).error(function(data, status) {
+    
     });
 }]);
 
